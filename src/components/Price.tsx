4e726987a5d30f2115bb02a3739be9d508e6d452
Andrew-devcoder <andrew.kovpak.01@gmail.com>
2025-06-17 20:17:27 +0200

    fix types

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -28,7 +28,7 @@ const tiers = [
 	},
 ];
 
-function classNames(...classes: any[]) {
+function classNames(...classes: (string | undefined | null | false)[]): string {
 	return classes.filter(Boolean).join(' ');
 }
 
